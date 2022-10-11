d4891ed8e6ce7424c2490765dce52cbb251a4d56
ChaoyiCheng2000 <ccy0105@seas.upenn.edu>
2022-10-11 18:41:40 -0400

    initialize home page

--- pokegram/src/components/home/home.js
+++ pokegram/src/components/home/home.js
@@ -1,3 +1,6 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 /* eslint-disable indent */
@@ -7,15 +10,38 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import ResponsiveAppBar from './menu';
 
 function Home(props) {
-    console.log(props);
+    // console.log(props);
+    const myUID = props.parStates.myUID;
+    const handleRootState = props.parStates.handleSetStates;
     const [isAct, setIsAct] = useState(false);
     const [isUp, setIsUp] = useState(false);
     const [isPosts, setIsPosts] = useState(true);
     const [isProf, setIsProf] = useState(false);
-    const [UID, setUID] = useState('');
+    const [UID, setUID] = useState(myUID);
 
     const theme = createTheme();
 
+    const handleHomeStates = (isLogOut, newIsProf, newIsAct, newIsUp, newIsPosts, newUID) => {
+        setIsAct(newIsAct);
+        setIsPosts(newIsPosts);
+        setUID(newUID);
+        setIsProf(newIsProf);
+        setIsUp(newIsUp);
+
+        // handle log out
+        if (isLogOut) {
+            handleRootState(false, true, '');
+        }
+    };
+    const parentStates = {
+        myUID,
+        isAct,
+        isUp,
+        isPosts,
+        isProf,
+        UID,
+        handleHomeStates
+    };
     function nextToRender() {
         let ret;
 
@@ -38,7 +64,8 @@ function Home(props) {
 
     return (
         <ThemeProvider theme={theme}>
-            <ResponsiveAppBar />
+            <ResponsiveAppBar homeStates={parentStates} />
+            {nextToRender()}
         </ThemeProvider>
     );
 }
--- pokegram/src/components/home/menu.js
+++ pokegram/src/components/home/menu.js
@@ -1,4 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
+/* eslint-disable comma-dangle */
 import * as React from 'react';
+import { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -11,35 +16,50 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+// import AdbIcon from '@mui/icons-material/Adb';
+import avatar from '../../images/pikachu.jpg';
 
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const pages = ['ACTIVITY', 'UPLOAD', 'LOGOUT'];
 
-const ResponsiveAppBar = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+function ResponsiveAppBar(props) {
+  console.log(props);
+  console.log(props.homeStates.UID);
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event) => {
+  const handleProfPicClick = (event) => {
     setAnchorElUser(event.currentTarget);
+    props.homeStates.handleHomeStates(false, true, false, false, false, props.UID);
   };
 
-  const handleCloseNavMenu = () => {
+  const handleCloseNavMenu = (event) => {
+    const nextPage = (event.target.textContent);
+    switch (nextPage) {
+      // set activity page
+      case pages[0]:
+        props.homeStates.handleHomeStates(false, false, true, false, false, props.homeStates.UID);
+        break;
+      // set upload page
+      case pages[1]:
+        props.homeStates.handleHomeStates(false, false, false, true, false, props.homeStates.UID);
+        break;
+      // set logout
+      case pages[2]:
+        props.homeStates.handleHomeStates(true, false, false, false, false, props.homeStates.UID);
+        break;
+      default:
+        break;
+    }
     setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
   };
 
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
@@ -55,7 +75,7 @@ const ResponsiveAppBar = () => {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            Pokegram
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -94,7 +114,6 @@ const ResponsiveAppBar = () => {
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
           <Typography
             variant="h5"
             noWrap
@@ -111,7 +130,7 @@ const ResponsiveAppBar = () => {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            Pokegram
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
@@ -127,36 +146,15 @@ const ResponsiveAppBar = () => {
 
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              <IconButton onClick={handleProfPicClick} sx={{ p: 0 }}>
+                <Avatar alt="profPic" src={avatar} />
               </IconButton>
             </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
           </Box>
         </Toolbar>
       </Container>
     </AppBar>
   );
-};
+}
+
 export default ResponsiveAppBar;
