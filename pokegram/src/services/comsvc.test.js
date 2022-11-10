bebaa7d42c84b4c69882ec3484acc212730903d7
ChaoyiCheng2000 <ccy0105@seas.upenn.edu>
2022-11-10 18:39:44 -0500

    add some tests

---
+++ pokegram/src/services/comsvc.test.js
@@ -0,0 +1,22 @@
+import { expect, test } from '@jest/globals';
+import commentService from './commentService';
+const axios = require("axios");
+const MockAdapter = require("axios-mock-adapter");
+
+const mockAxios = new MockAdapter(axios);
+
+describe("get Comm List Test", () => {
+  mockAxios.onGet().reply(200, [{
+    id:1,
+    postId:2,
+    timestamp: '2022-11-10',
+    content: 'test',
+    referredUser: [],
+    commentorid: 1
+  }]);
+  test("test comments", () => {
+    commentService.getCommentBypostId(2).then((data)=> {
+      expect(data.length).toBe(1);
+    });
+  });
+});
