71dcc5d6a5bfe6b1390343f4db5c92da0fba8c4c
YuRenee <shuyany@seas.upenn.edu>
2022-12-18 09:18:42 +0800

    e2e start

--- pokegram/cypress/e2e/registration.cy.js
+++ pokegram/cypress/e2e/registration.cy.js
@@ -1,6 +1,23 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000');
-    cy.get('button');
+    /*
+    cy.get('button').contains('Sign Up');
+    cy.get('button').contains('Sign Up').click();
+    cy.get('#fullname').type('cypress test').should('have.value', 'cypress test');
+    cy.get('#username').type('cypress').should('have.value', 'cypress');
+    cy.get('#password').type('password').should('have.value', 'password');
+    cy.get('#emailaddress').type('cypress@upenn.edu').should('have.value', 'cypress@upenn.edu');
+    cy.get('button').contains('Sign Up').click();
+    cy.get('button').contains('Sign In').click();
+    */
+    // cy.get('#username').type('cypress').should('have.value', 'cypress');
+    // cy.get('#password').type('password').should('have.value', 'password');
+    cy.get('#username').type('testFosug').should('have.value', 'testFosug');
+    cy.get('#password').type('testFosug').should('have.value', 'testFosug');
+    cy.get('button').contains('Sign In').click();
+    cy.get('button').contains('ACTIVITY');
+    // cy.get('button').contains('LOGOUT').click();
+    cy.contains('.MuiCard-root');
   })
 })
